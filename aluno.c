fae9ced6d6c052c9c5be167cb6e6eb73867287a1
rafadonas <rafadonas.dev@gmail.com>
2025-09-02 00:07:09 -0300

    feat: Ex03 Implement student search by RM

--- aluno.c
+++ aluno.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "aluno.h"
+#include "lista.h"
 Aluno criarAluno(const char *nome, int rm, float nota) {
     Aluno a;
     strncpy(a.nome, nome, sizeof(a.nome));
@@ -10,4 +11,12 @@ Aluno criarAluno(const char *nome, int rm, float nota) {
 }
 void imprimirAluno(Aluno a) {
     printf("Nome: %s | RM: %d | Nota: %.2f\n", a.nome, a.rm, a.nota);
+}
+int buscarAlunoPorRM(Lista l, int rm) {
+    for (int i = 0; i < l.tamanho; i++) {
+        if (l.dados[i].rm == rm) {
+            return i;
+        }
+    }
+    return -1;
 }
--- aluno.h
+++ aluno.h
@@ -1,11 +1,17 @@
 #ifndef ALUNO_H
 #define ALUNO_H
+
 typedef struct {
     char nome[50];
     int rm;
     float nota;
 } Aluno;
+
+#include "lista.h"
+
 // Operações do TAD
 Aluno criarAluno(const char *nome, int rm, float nota);
 void imprimirAluno(Aluno a);
+int buscarAlunoPorRM(Lista l, int rm);
+
 #endif
